25d320f91c0484d1e755073fb89b4d8aaa90a167
jiachangsheng <jiachangsheng2020@gmail.com>
2025-05-16 16:47:20 +0800

    📝 更新于 2025-05-16 16:47:19

--- install.ps1
+++ install.ps1
@@ -1,1 +1,8 @@
-scoop install git neovim pwsh python nodejs curl wget unzip 7zip
+scoop bucket add main extras nerd-fonts games
+scoop install git neovim pwsh python nodejs curl wget unzip 7zip gcc
+scoop install chromium listary hibit-uninstaller
+
+scoop install zebar translucenttb vitrite glazewm
+
+scoop install qq wechat telegram epic-game-launcher jetbrains-toolbox
+
---
+++ install_1.ps1
@@ -0,0 +1,19 @@
+scoop install extras/notepadplusplus
+
+scoop install extras/vcredist2022
+
+scoop install extras/vscode
+
+scoop install extras/sublime-text
+
+winget install Youdao.YoudaoTranslate
+
+scoop install extras/processhacker
+
+scoop install extras/xmousebuttoncontrol
+
+scoop install extras/ollama-full
+
+scoop install extras/binance
+
+winget install Chocolatey.Chocolatey
