04d825f5e31b8a202c5928455580710b851aa902
jiachangsheng <jiachangsheng2020@gmail.com>
2025-05-27 09:08:56 +0800

    📝 更新于 2025-05-27 09:08:55

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ scoop bucket add main extras nerd-fonts games
 scoop install git neovim pwsh python nodejs curl wget unzip 7zip gcc
 scoop install chromium listary hibit-uninstaller
 
-scoop install zebar translucenttb vitrite glazewm
+scoop install zebar translucenttb glazewm
 
 scoop install qq wechat telegram  jetbrains-toolbox
 
