1e441b3dc3fa5392078eb54197edff0e58e2c0c0
jiachangsheng <jiachangsheng2020@gmail.com>
2025-06-03 20:26:15 +0800

    📝 更新于 2025-06-03 20:26:14

--- install.ps1
+++ install.ps1
@@ -1,12 +1,23 @@
 scoop bucket add main extras nerd-fonts games
-scoop install git neovim pwsh python nodejs curl wget unzip 7zip gcc
-scoop install brave listary hibit-uninstaller
+
+scoop install git neovim pwsh python nodejs curl wget unzip 7zip gcc make
+scoop install listary hibit-uninstaller
+
+scoop install extras/brave
+
 
 scoop install zebar translucenttb glazewm
+
+scoop install extras/autohotkey
 
 scoop install qq wechat telegram  jetbrains-toolbox
 
 scoop install nvm fzf zoxide vieb
+
+# 开源音频管理器
+scoop bucket add nonportable
+scoop install nonportable/equalizer-apo-np
+scoop install nonportable/peace-np
 
 Install-Module posh-git -Scope CurrentUser -Force
 Install-Module oh-my-posh -Scope CurrentUser -Force
@@ -15,3 +26,23 @@ Install-Module -Name Z -Force
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name PSFzf -Scope CurrentUser -Force
+
+
+
+# scoop install extras/notepadplusplus
+#
+# scoop install extras/vcredist2022
+#
+# scoop install extras/vscode
+#
+# scoop install extras/sublime-text
+#
+# winget install Youdao.YoudaoTranslate
+#
+# scoop install extras/processhacker
+#
+# scoop install extras/xmousebuttoncontrol
+#
+# scoop install extras/ollama-full
+#
+# winget install Chocolatey.Chocolatey
