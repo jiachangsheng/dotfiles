7a7f4a6f2d77420f0a4aa5ec9b6e354b774ddb1f
jiachangsheng <jiachangsheng2020@gmail.com>
2025-06-03 17:50:50 +0800

    📝 更新于 2025-06-03 17:50:49

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,5 +2,17 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.relativenumber = true
+vim.o.cursorlineopt ='both'
+-- vim.cmd("highlight Normal guibg=NONE")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+
+
