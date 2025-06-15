d879990bf8f0333321874bf5c95f8bd5c8a501bc
jiachangsheng <jia.cs@icloud.com>
2025-06-15 23:04:13 +0800

    📝 更新于 2025-06-15 23:04:08

--- nvim/lk.ps1
+++
@@ -1,6 +0,0 @@
-# 先备份原配置目录（如果存在）
-# Rename-Item "$env:LOCALAPPDATA\nvim" "nvim_backup" -ErrorAction SilentlyContinue
-
-# 创建 nvim 目录软链接
-
-New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Target "$pwd"
