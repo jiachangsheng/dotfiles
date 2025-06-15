93c08342889c03dc148ea9bd0cfc5ec9d6c6fc91
jiachangsheng <jia.cs@icloud.com>
2025-06-16 00:24:58 +0800

    📝 更新于 2025-06-16 00:24:53

--- .emacs-profiles.el
+++
@@ -1,9 +0,0 @@
-(
- ("default" . ((user-emacs-directory . "~/prelude")))
-
- ("doom" .
-  ((user-emacs-directory . "~/doom-emacs")
-   (env . (("DOOMDIR" . "~/.doom.d")))))
-
- ("space" . ((user-emacs-directory . "~/spacemacs")))
-)
